df40f16e97dd7d22d442052b458cab0fd8e3e4f3
robreder <r.reder@gmx.at>
2025-09-30 15:09:32 +0200

    added

---
+++ clean-docker.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+docker compose down
+docker container prune -f
+docker image prune -f
+docker volume prune -f
+docker builder prune --all --force #build cache löschen
+IMAGES=$(docker images -q)
+
+for image in $IMAGES
+do
+    docker image rm -f $image
+done
+VOLUMES=$(docker volume ls -q)
+for volume in $VOLUMES
+do
+    docker volume rm $volume 
+done
+docker container ls
+docker volume ls
+docker image ls
+echo Fertig!
